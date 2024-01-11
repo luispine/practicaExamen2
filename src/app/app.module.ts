dedcaf8d2b48ee862cb54131fcee1d741765e3b6
luiVaz <vazquezcedar@outlook.com>
2024-01-10 19:09:31 -0600

    agrego componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormulaComponent } from './formula/formula.component';
+import { UiComponent } from './ui/ui.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FormulaComponent
+    FormulaComponent,
+    UiComponent
   ],
   imports: [
     BrowserModule,
