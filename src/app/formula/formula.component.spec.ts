2c66db46bef14f86e8ef07a942db38b203eaf6b7
luiVaz <vazquezcedar@outlook.com>
2024-01-10 18:43:46 -0600

    creacion de la rama unit-test y los componentes

---
+++ src/app/formula/formula.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { FormulaComponent } from './formula.component';
+
+describe('FormulaComponent', () => {
+  let component: FormulaComponent;
+  let fixture: ComponentFixture<FormulaComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [FormulaComponent]
+    });
+    fixture = TestBed.createComponent(FormulaComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/formula/formula.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-formula',
+  templateUrl: './formula.component.html',
+  styleUrls: ['./formula.component.css']
+})
+export class FormulaComponent {
+
+}
